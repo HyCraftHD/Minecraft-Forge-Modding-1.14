fe8e5c7205c1608ffb7bab8e87bee1ff6d811a2a
HyCraftHD <hycrafthd@live.de>
2019-09-07 21:53:26 +0200

    Block (6)

--- src/main/java/net/hycrafthd/tutorialmod/init/TutorialModBlocks.java
+++ src/main/java/net/hycrafthd/tutorialmod/init/TutorialModBlocks.java
@@ -1,22 +1,33 @@
 package net.hycrafthd.tutorialmod.init;
 
 import net.hycrafthd.tutorialmod.TutorialMod;
+import net.hycrafthd.tutorialmod.block.BasicBlock;
+import net.hycrafthd.tutorialmod.item.BasicBlockItem;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @EventBusSubscriber(modid = TutorialMod.MODID, bus = Bus.MOD)
 public class TutorialModBlocks {
 
+	public static final BasicBlock BASIC_BLOCK = new BasicBlock();
+
 	@SubscribeEvent
 	public static void register(Register<Block> event) {
+		final IForgeRegistry<Block> registry = event.getRegistry();
+
+		registry.register(BASIC_BLOCK.setRegistryName(TutorialMod.MODID, "basic_block"));
 	}
 
 	@SubscribeEvent
-	public static void registerItemBlock(Register<Item> event) {
+	public static void registerBlockItem(Register<Item> event) {
+		final IForgeRegistry<Item> registry = event.getRegistry();
+
+		registry.register(new BasicBlockItem(BASIC_BLOCK));
 	}
 
 }
---
+++ src/main/java/net/hycrafthd/tutorialmod/item/BasicBlockItem.java
@@ -0,0 +1,14 @@
+package net.hycrafthd.tutorialmod.item;
+
+import net.hycrafthd.tutorialmod.init.TutorialModItemGroups;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+
+public class BasicBlockItem extends BlockItem {
+
+	public BasicBlockItem(Block block) {
+		super(block, new Properties().group(TutorialModItemGroups.GROUP));
+		setRegistryName(block.getRegistryName());
+	}
+
+}
