defa91e9ea24362925a457f35ef60636e792355a
HyCraftHD <hycrafthd@live.de>
2019-09-07 16:52:07 +0200

    Item group (4)

---
+++ src/main/java/net/hycrafthd/tutorialmod/init/TutorialModItemGroups.java
@@ -0,0 +1,10 @@
+package net.hycrafthd.tutorialmod.init;
+
+import net.hycrafthd.tutorialmod.itemgroup.BasicItemGroup;
+import net.minecraft.block.Blocks;
+
+public class TutorialModItemGroups {
+
+	public static final BasicItemGroup GROUP = new BasicItemGroup("group", () -> Blocks.DAMAGED_ANVIL);
+
+}
